87b8f92fb58438f02ee5e67e96e80de56e5a6ecf
Matheus Pupo <matheusmpupo@gmail.com>
2019-10-02 09:04:19 -0300

    Tela de login nova

--- Interface/login.py
+++ Interface/login.py
@@ -1,88 +1,156 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'testeqt.ui'
+# Form implementation generated from reading ui file 'login.ui'
 #
-# Created by: PyQt5 UI code generator 5.13.0
+# Created by: PyQt5 UI code generator 5.10.1
 #
 # WARNING! All changes made in this file will be lost!
-
 
 from PyQt5 import QtCore, QtGui, QtWidgets
 import os
 
-class Ui_Dialog(object):
-    def setupUi(self, Dialog):
-        Dialog.setObjectName("Dialog")
-        Dialog.resize(400, 237)
-        self.label = QtWidgets.QLabel(Dialog)
-        self.label.setGeometry(QtCore.QRect(30, 70, 58, 18))
-        self.label.setObjectName("label")
-        self.label_2 = QtWidgets.QLabel(Dialog)
-        self.label_2.setGeometry(QtCore.QRect(30, 110, 58, 18))
-        self.label_2.setObjectName("label_2")
-        self.label_3 = QtWidgets.QLabel(Dialog)
-        self.label_3.setGeometry(QtCore.QRect(150, 20, 141, 31))
-        self.label_3.setObjectName("label_3")
-        self.lineEdit = QtWidgets.QLineEdit(Dialog)
-        self.lineEdit.setGeometry(QtCore.QRect(80, 70, 251, 21))
-        self.lineEdit.setInputMask("")
-        self.lineEdit.setText("")
-        self.lineEdit.setEchoMode(QtWidgets.QLineEdit.Normal)
-        self.lineEdit.setPlaceholderText("")
-        self.lineEdit.setObjectName("lineEdit")
-        self.lineEdit_2 = QtWidgets.QLineEdit(Dialog)
-        self.lineEdit_2.setGeometry(QtCore.QRect(80, 110, 251, 21))
-        self.lineEdit_2.setEchoMode(QtWidgets.QLineEdit.Password)
-        self.lineEdit_2.setObjectName("lineEdit_2")
-        self.pushButton = QtWidgets.QPushButton(Dialog)
-        self.pushButton.setGeometry(QtCore.QRect(160, 170, 88, 34))
-        self.pushButton.setCheckable(False)
-        self.pushButton.setChecked(False)
-        self.pushButton.setObjectName("pushButton")
-        self.pushButton.clicked.connect(self.verifica_senha)                                                                    
-        self.label_4 = QtWidgets.QLabel(Dialog)
-        self.label_4.setGeometry(QtCore.QRect(0, 140, 401, 20))
-        self.label_4.setAlignment(QtCore.Qt.AlignCenter)
-        self.label_4.setText("")
-        self.label_4.setObjectName("label_4")
-        self.actionVerificaSenha = QtWidgets.QAction(Dialog)
-        self.actionVerificaSenha.setObjectName("actionVerificaSenha")
-        self.retranslateUi(Dialog)
-        QtCore.QMetaObject.connectSlotsByName(Dialog)
-    #aplica os textos nas labels
-    def retranslateUi(self, Dialog):
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(1280, 720)
+        MainWindow.setMinimumSize(QtCore.QSize(1280, 720))
+        font = QtGui.QFont()
+        font.setFamily("OpenSymbol")
+        MainWindow.setFont(font)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setMinimumSize(QtCore.QSize(1280, 720))
+        self.centralwidget.setObjectName("centralwidget")
+        self.verticalLayout_4 = QtWidgets.QVBoxLayout(self.centralwidget)
+        self.verticalLayout_4.setObjectName("verticalLayout_4")
+        spacerItem = QtWidgets.QSpacerItem(20, 40, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
+        self.verticalLayout_4.addItem(spacerItem)
+        spacerItem1 = QtWidgets.QSpacerItem(20, 40, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
+        self.verticalLayout_4.addItem(spacerItem1)
+        self.layoutTexto = QtWidgets.QVBoxLayout()
+        self.layoutTexto.setObjectName("layoutTexto")
+        self.labelLogin = QtWidgets.QLabel(self.centralwidget)
+        font = QtGui.QFont()
+        font.setFamily("Nimbus Roman No9 L")
+        font.setPointSize(22)
+        font.setBold(True)
+        font.setWeight(75)
+        self.labelLogin.setFont(font)
+        self.labelLogin.setObjectName("labelLogin")
+        self.layoutTexto.addWidget(self.labelLogin, 0, QtCore.Qt.AlignHCenter|QtCore.Qt.AlignVCenter)
+        self.verticalLayout_4.addLayout(self.layoutTexto)
+        spacerItem2 = QtWidgets.QSpacerItem(20, 40, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
+        self.verticalLayout_4.addItem(spacerItem2)
+        self.layoutInput = QtWidgets.QGridLayout()
+        self.layoutInput.setObjectName("layoutInput")
+        self.lineEditUsername = QtWidgets.QLineEdit(self.centralwidget)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Preferred, QtWidgets.QSizePolicy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.lineEditUsername.sizePolicy().hasHeightForWidth())
+        self.lineEditUsername.setSizePolicy(sizePolicy)
+        self.lineEditUsername.setMinimumSize(QtCore.QSize(350, 0))
+        self.lineEditUsername.setMaximumSize(QtCore.QSize(350, 16777215))
+        self.lineEditUsername.setObjectName("lineEditUsername")
+        self.layoutInput.addWidget(self.lineEditUsername, 0, 2, 1, 1, QtCore.Qt.AlignLeft)
+        self.lineEditPassword = QtWidgets.QLineEdit(self.centralwidget)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Preferred, QtWidgets.QSizePolicy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.lineEditPassword.sizePolicy().hasHeightForWidth())
+        self.lineEditPassword.setSizePolicy(sizePolicy)
+        self.lineEditPassword.setMinimumSize(QtCore.QSize(350, 0))
+        self.lineEditPassword.setMaximumSize(QtCore.QSize(350, 16777215))
+        self.lineEditPassword.setEchoMode(QtWidgets.QLineEdit.Password)
+        self.lineEditPassword.setObjectName("lineEditPassword")
+        self.layoutInput.addWidget(self.lineEditPassword, 1, 2, 1, 1, QtCore.Qt.AlignLeft)
+        self.labelUsername = QtWidgets.QLabel(self.centralwidget)
+        self.labelUsername.setMinimumSize(QtCore.QSize(0, 0))
+        self.labelUsername.setMaximumSize(QtCore.QSize(100, 16777215))
+        font = QtGui.QFont()
+        font.setFamily("Nimbus Roman No9 L")
+        font.setPointSize(12)
+        self.labelUsername.setFont(font)
+        self.labelUsername.setObjectName("labelUsername")
+        self.layoutInput.addWidget(self.labelUsername, 0, 1, 1, 1, QtCore.Qt.AlignHCenter)
+        spacerItem3 = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
+        self.layoutInput.addItem(spacerItem3, 1, 0, 1, 1)
+        spacerItem4 = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
+        self.layoutInput.addItem(spacerItem4, 0, 3, 1, 1)
+        spacerItem5 = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
+        self.layoutInput.addItem(spacerItem5, 0, 0, 1, 1)
+        self.labelPassword = QtWidgets.QLabel(self.centralwidget)
+        self.labelPassword.setMinimumSize(QtCore.QSize(0, 0))
+        self.labelPassword.setMaximumSize(QtCore.QSize(100, 16777215))
+        font = QtGui.QFont()
+        font.setFamily("Nimbus Roman No9 L")
+        font.setPointSize(12)
+        self.labelPassword.setFont(font)
+        self.labelPassword.setObjectName("labelPassword")
+        self.layoutInput.addWidget(self.labelPassword, 1, 1, 1, 1, QtCore.Qt.AlignHCenter)
+        spacerItem6 = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
+        self.layoutInput.addItem(spacerItem6, 1, 3, 1, 1)
+        spacerItem7 = QtWidgets.QSpacerItem(60, 20, QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Minimum)
+        self.layoutInput.addItem(spacerItem7, 0, 4, 1, 1)
+        spacerItem8 = QtWidgets.QSpacerItem(60, 20, QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Minimum)
+        self.layoutInput.addItem(spacerItem8, 1, 4, 1, 1)
+        self.verticalLayout_4.addLayout(self.layoutInput)
+        spacerItem9 = QtWidgets.QSpacerItem(20, 40, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
+        self.verticalLayout_4.addItem(spacerItem9)
+        self.signInButton = QtWidgets.QPushButton(self.centralwidget)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Preferred, QtWidgets.QSizePolicy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.signInButton.sizePolicy().hasHeightForWidth())
+        self.signInButton.setSizePolicy(sizePolicy)
+        self.signInButton.setMinimumSize(QtCore.QSize(150, 0))
+        self.signInButton.setMaximumSize(QtCore.QSize(150, 16777215))
+        font = QtGui.QFont()
+        font.setFamily("Nimbus Roman No9 L")
+        font.setPointSize(11)
+        font.setBold(True)
+        font.setWeight(75)
+        self.signInButton.setFont(font)
+        self.signInButton.setObjectName("signInButton")
+        self.signInButton.setCheckable(False)
+        self.signInButton.setChecked(False)
+        self.signInButton.clicked.connect(self.verifica_senha) 
+        self.verticalLayout_4.addWidget(self.signInButton, 0, QtCore.Qt.AlignHCenter)
+        spacerItem10 = QtWidgets.QSpacerItem(20, 40, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
+        self.verticalLayout_4.addItem(spacerItem10)
+        spacerItem11 = QtWidgets.QSpacerItem(20, 40, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
+        self.verticalLayout_4.addItem(spacerItem11)
+        MainWindow.setCentralWidget(self.centralwidget)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
-        Dialog.setWindowTitle(_translate("Dialog", "Login"))
-        self.label.setText(_translate("Dialog", "Nome:"))
-        self.label_2.setText(_translate("Dialog", "Senha:"))
-        self.label_3.setText(_translate("Dialog", "<html><head/><body><p><span style=\" font-weight:600;\">Dados do Usuário</span></p></body></html>"))
-        self.pushButton.setText(_translate("Dialog", "Login"))
-        self.actionVerificaSenha.setText(_translate("Dialog", "VerificaSenha"))
-        
-    #verifica senha tela 1
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.labelLogin.setText(_translate("MainWindow", "Sign In"))
+        self.labelUsername.setText(_translate("MainWindow", "Username:"))
+        self.labelPassword.setText(_translate("MainWindow", "Password:"))
+        self.signInButton.setText(_translate("MainWindow", "Sign In"))
+
     def verifica_senha(self):  
         _translate = QtCore.QCoreApplication.translate
-        Dialog.setWindowTitle(_translate("Dialog", "Dialog"))
-        if self.lineEdit.text() == "adm123" and self.lineEdit_2.text() == "adm123":
-            self.label_4.setText(_translate("Dialog", "Entrou cornao"))
-            Dialog.hide()
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        if self.lineEditUsername.text() == "adm123" and self.lineEditPassword.text() == "adm123":
+            MainWindow.hide()
             import os 
-            os.system("python3 main.py " + self.lineEdit.text())
+            os.system("python3 main.py " + self.lineEditUsername.text())
             sys.exit(app.exec_())
 
-        else:
-            self.label_4.setText(_translate("Dialog", "Senha Incorreta!"))
-            
-        
+        #else:
+            #self.label_4.setText(_translate("MainWindow", "Senha Incorreta!"))
 
 
 if __name__ == "__main__":
     import sys
     app = QtWidgets.QApplication(sys.argv)
-    Dialog = QtWidgets.QDialog()
-    ui = Ui_Dialog()
-    ui.setupUi(Dialog)
-    Dialog.show()
-    
-    
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
     sys.exit(app.exec_())
+
